36abbec1c60243e577a55ca0269b4f6463459cac
Padmarani <padmaranipadasalagi9@gmail.com>
2022-07-28 00:06:39 -0500

    Second Commit

--- src/main/java/PomClasses/LoginPage.java
+++ src/main/java/PomClasses/LoginPage.java
@@ -16,12 +16,15 @@ public class LoginPage extends Util1{
 	
 	//WebElements	
 	@FindBy(xpath="(//input[@type='text'])[2]")
+	
 	private WebElement emailField;
 	
 	@FindBy(xpath="//input[@type='password']")
+	
 	private WebElement passwordField;
 	
 	@FindBy(xpath="(//button[@type='submit'])[2]")
+	
 	private WebElement loginBtn;
 	
 	
